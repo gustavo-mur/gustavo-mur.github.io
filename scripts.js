1f9fa9b221bf1f892c417b7b2041089301df9cf2
gustavo-mur <gustavo.murguia10@gmail.com>
2021-01-25 12:56:26 -0400

    Init project

---
+++ scripts.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+  });
+
+   //var collapsibleElem = document.querySelector('.collapsible');
+   //var collapsibleInstance = M.Collapsible.init(collapsibleElem, {});
